b64017aa0d1a094af525582b7fee15b5371909db
rjosula <rjosula@10.3.30.67>
2020-01-06 11:45:41 +0530

    Initial Code

---
+++ src/main/java/com/voter/app/consumer/VoterConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package com.voter.app.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoterConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoterConsumerAppApplication.class, args);
+	}
+
+}
